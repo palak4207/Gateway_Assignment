cd6c6163c77afbde214da09d488aae58d2ba437f
Palak-Agrawal05 <78787280+Palak-Agrawal05@users.noreply.github.com>
2021-03-30 16:13:03 +0530

    commit

---
+++ Testing Assignment/Testing Assignment 1/TestingPassengerDemo/Controllers/PassengerController.cs
@@ -0,0 +1,86 @@
+﻿using TestingPassengerDemo.Models;
+
+using CLPM.DAL.Repository.Interface;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestingPassengerDemo.Controllers
+{
+    public class PassengerController : ApiController
+    {
+        private readonly IPassengerRepository _passengerManager;
+
+        public PassengerController(IPassengerRepository passengerManager)
+        {
+            _passengerManager = passengerManager;
+        }
+
+        [HttpPost]
+        [Route("api/Passengers/PassengerRegistration")]
+        public Passenger PassengerRegistration([FromBody] Passenger model)
+        {
+            return _passengerManager.PassengerRegistration(model);
+        }
+
+        [HttpPut]
+        [Route("api/Passengers/PassengerDetailsUpdate")]
+        public Passenger PassengerDetailsUpdate([FromBody] Passenger model)
+        {
+
+            var result = _passengerManager.PassengerDetailsUpdate(model);
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("api/Passengers/PassengerDetailsDelete")]
+        public bool PassengerDetailsDelete(String id)
+        {
+
+            bool result = _passengerManager.PassengerDetailsDelete(id);
+            return result;
+        }
+
+        [HttpGet]
+        [Route("api/Passengers/GetPassenger")]
+        public HttpResponseMessage GetPassenger(String id)
+        {
+            if (id != null)
+            {
+                try
+                {
+
+                    var passenger = _passengerManager.getPassengerByPassengerId(id);
+                    if (passenger.FirstName != null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, passenger);
+
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.InternalServerError);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError);
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        [HttpGet]
+        [Route("api/Passengers/getAllPassenger")]
+        public IList<Passenger> getAllPassenger()
+        {
+            return _passengerManager.getAllPassenger();
+        }
+    }
+}
+
---
+++ Testing Assignment/Testing Assignment 1/TestingPassengerDemo_Test/UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Http.Results;
+using TestingPassengerDemo.Controllers;
+using TestingPassengerDemo.Models;
+using CLPM.DAL.Repository.Interface;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+using System;
+
+namespace CLPM_Test
+{
+    public class UnitTest1
+    {
+        private readonly Mock<IPassengerRepository> mockDataRepo = new Mock<IPassengerRepository>();
+        private readonly PassengerController _passengerController;
+
+        public UnitTest1()
+        {
+            _passengerController = new PassengerController(mockDataRepo.Object);
+        }
+
+        [Fact]
+        public void Test_GetAllPassengers()
+        {
+            //Arrange
+            var resultType = mockDataRepo.Setup(x => x.getAllPassenger()).Returns(getPassengerList());
+            //Act
+            var response = _passengerController.getAllPassenger();
+            //Assert
+            Assert.Equal(2, response.Count);
+        }
+
+        [Fact]
+        public void Test_GetUserById()
+        {
+            //Arrange
+            var passenger = new Passenger();
+            passenger.PassengerId = Convert.ToInt32(DateTime.Now.Ticks.ToString());
+
+            var resultType = mockDataRepo.Setup(x => x.getPassengerByPassengerId(passenger.PassengerId.ToString())).Returns(passenger);
+            //Act
+            var result = _passengerController.GetPassenger(passenger.PassengerId.ToString());
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Test_AddPassenger()
+        {
+            //Arrange
+            var newpassenger = new Passenger() { PassengerId = Convert.ToInt32(DateTime.Now.Ticks.ToString()), FirstName = "Palak", LastName = "Agrawal", PhoneNumber = "9664560601" };
+            var response = mockDataRepo.Setup(x => x.PassengerRegistration(newpassenger)).Returns(AddPassenger());
+            //Act
+            var result = _passengerController.PassengerRegistration(newpassenger);
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Test_UpdatePassenger()
+        {
+            //Act
+            var model = JsonConvert.DeserializeObject<Passenger>(File.ReadAllText("Data/UpdatePassenger.json"));
+            //Arrange
+            var result = mockDataRepo.Setup(x => x.PassengerDetailsUpdate(model)).Returns(model);
+            var response = _passengerController.PassengerDetailsUpdate(model);
+            //Assert
+            Assert.Equal(model, response);
+        }
+
+        [Fact]
+        public void Test_DeletePassenger()
+        {
+            //Arrange
+            var passenger = new Passenger();
+            passenger.PassengerId = Convert.ToInt32(DateTime.Now.Ticks.ToString());
+            var resultType = mockDataRepo.Setup(x => x.PassengerDetailsDelete(passenger.PassengerId.ToString())).Returns(true);
+            //Act
+            var response = _passengerController.PassengerDetailsDelete(passenger.PassengerId.ToString());
+            //Assert
+            Assert.True(response);
+        }
+
+        private static IList<Passenger> getPassengerList()
+        {
+            IList<Passenger> passengers = new List<Passenger>()
+            {
+                new Passenger(){PassengerId=Convert.ToInt32(DateTime.Now.Ticks.ToString()),FirstName="Palak",LastName="Agrawal",PhoneNumber="96644530601"},
+                new Passenger(){PassengerId=Convert.ToInt32(DateTime.Now.Ticks.ToString()),FirstName="Palak",LastName="Agrawal",PhoneNumber="96644530601"}
+            };
+            return passengers;
+        }
+
+
+        private static Passenger AddPassenger()
+        {
+            var passenger = new Passenger() { PassengerId = Convert.ToInt32(DateTime.Now.Ticks.ToString()), FirstName = "Palak", LastName = "Agrawal", PhoneNumber = "9664560601" };
+            return passenger;
+        }
+    }
+}
