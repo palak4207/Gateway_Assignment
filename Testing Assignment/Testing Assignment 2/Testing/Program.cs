7c2ec143cccc16ae8bf4c96a28ad2a1f967c3efd
palak4207 <63452981+palak4207@users.noreply.github.com>
2021-04-02 10:47:21 +0530

    Update Program.cs

--- Testing Assignment/Testing Assignment 2/Testing/Program.cs
+++ Testing Assignment/Testing Assignment 2/Testing/Program.cs
@@ -12,34 +12,34 @@ namespace Testing
         {
             string input = Console.ReadLine();
 
-            //Case 1  Return a copy of given input string with uppercase characters converted to lowercase and viceversa.
+            // Case 1  Return a copy of given input string with uppercase characters converted to lowercase and viceversa.
             Console.WriteLine("String with uppercase characters converted to lowercase : " + input.AddLowerCase());
 
-            //Case 2 Return a copy of given input string with lowercase characters converted to Uppercase.
+            // Case 2 Return a copy of given input string with lowercase characters converted to Uppercase.
             Console.WriteLine("String with lowercase characters converted to uppercase : " + input.AddUpperCase());
 
-            //Case 3  Converts the specified string to title case .
+            // Case 3  Converts the specified string to title case .
             Console.WriteLine("String with title case : " + input.TitleCase());
 
-            //Case 4  All the characters from given input string are in lower case or not 
+            // Case 4  All the characters from given input string are in lower case or not 
             Console.WriteLine("To check string is in lower case or not : " + input.CheckLowerCase());
 
-            //Case 5 To convert the  first character into  upper case and the rest lower case.
+            // Case 5 To convert the  first character into  upper case and the rest lower case.
             Console.WriteLine("To convert the first charcter into upper case : " + input.FirstUpperLetter());
 
-            //Case 6 all the characters from given input string are in upper case or not
+            // Case 6 all the characters from given input string are in upper case or not
             Console.WriteLine("To check all the characters are in upper case or not : " + input.CheckUpperCase());
 
-            //Case 7    Function to identify whether given input string can be converted to a valid numeric value or not. 
+            // Case 7    Function to identify whether given input string can be converted to a valid numeric value or not. 
             Console.WriteLine("To check whether string can be converted to a numreic value : " + input.NumberValidation());
 
-            //Case 8 Function to remove the last character from given the string
+            // Case 8 Function to remove the last character from given the string
             Console.WriteLine("To remove the last character form the string : " + input.LastCharacterRemove());
 
-            //Case 9 Get the word count from an input string.
+            // Case 9 Get the word count from an input string.
             Console.WriteLine("To count the word : " + input.WordCount());
 
-            //Case 10     Convert an input string to integer.   
+            // Case 10     Convert an input string to integer.   
             Console.WriteLine("To convert string to int : " + input.StringToInt());
 
         }
